6b041eeb38d7ae4d5e747be4d0fc6cd2cb5d9025
Miguel Rodriguez <miglos@hotmail.es>
2017-05-15 02:06:32 +0200

    Nav menu partials created. No delete task yet

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,26 +4,23 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">My Tasks</div>
-
-                    <div class="panel-body">
-                        @foreach ($tasks as $task)
-                            <article>
-                                <a href="{{ $task->path() }}">
-                                    <span class="label label-info pull-right" style="padding: 5px 10px; margin-left: 1em;">{{ $task->posts_count }} {{ str_plural('comentario', $task->posts_count) }}</span>
+                @foreach ($tasks as $task)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <span class="label label-info pull-right">
+                                <a href="{{ $task->path() }}" style="text-decoration: none; color: #c4e3f3">
+                                    {{ $task->posts_count }} {{ str_plural('comentario', $task->posts_count) }}
                                 </a>
-                                <h4>
-                                    <a href="{{ $task->path() }}">
-                                        {{ $task->description }}
-                                    </a>
-                                </h4>
-                                <div class="body">El nombre del cliente es {{ $task->client_name }} y su teléfono {{ $task->client_phone }}</div>
-                            </article>
-                            <hr>
-                        @endforeach
+                            </span>
+                            <a href="{{ $task->path() }}">
+                                {{ $task->description }}
+                            </a>
+                        </div>
+                        <div class="panel-body">
+                            <div class="body">El nombre del cliente es {{ $task->client_name }} y su teléfono {{ $task->client_phone }}</div>
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
